1b9d4414fd65a9dc1b7436c08125bd8ec7bb1a2a
jkidwell <jay@contensive.com>
2020-01-31 00:37:26 -0500

    close

--- source/aoHtmlImport/Controllers/ImportController.cs
+++ source/aoHtmlImport/Controllers/ImportController.cs
@@ -18,56 +18,108 @@ namespace Contensive.Addons.HtmlImport {
             /// <param name="cp"></param>
             /// <param name="htmlSourcePathFilename"></param>
             /// <returns></returns>
-            public static bool importHtmlFile(CPBaseClass cp, string htmlSourcePathFilename, string layoutName, string templateName, ref string returnErrorMessage) {
+            public static bool importHtmlFile(CPBaseClass cp, string htmlSourcePathFilename, int layoutId, int templateId, ref string returnStatusMessage) {
                 try {
+                    returnStatusMessage = "";
                     HtmlDocument htmlDoc = new HtmlDocument();
                     htmlDoc.Load(htmlSourcePathFilename);
-                    returnErrorMessage = default;
+                    if (htmlDoc == null) {
+                        //
+                        // -- body tag not found, import the whole document
+                        returnStatusMessage += cp.Html.p("The uploaded file is empty.");
+                        return false;
+                    }
+                    //
+                    // -- search for meta name=template|layout content=recordaname
+                    string layoutRecordName = string.Empty;
+                    string templateRecordName = string.Empty;
+                    var metadataList = htmlDoc.DocumentNode.SelectNodes("//meta");
+                    if (metadataList != null) {
+                        foreach (var metadataNode in metadataList) {
+                            switch (metadataNode.GetAttributeValue("name", String.Empty).ToLowerInvariant()) {
+                                case "layout": {
+                                        layoutRecordName = metadataNode.GetAttributeValue("content", String.Empty);
+                                        break;
+                                    }
+                                case "template": {
+                                        templateRecordName = metadataNode.GetAttributeValue("content", String.Empty);
+                                        break;
+                                    }
+                                default: {
+                                        break;
+                                    }
+                            }
+                        }
+                    }
                     //
                     // -- get body
                     HtmlAgilityPack.HtmlNode bodyNode = htmlDoc.DocumentNode.SelectSingleNode("//body");
-                    string body = bodyNode.InnerText;
-                    //
-                    // -- search for meta name=template|layout content=recordaname
-                    var metadataList = htmlDoc.DocumentNode.SelectNodes("//meta");
-                    string layoutRecordName = layoutName;
-                    string templateRecordName = templateName;
-                    foreach (var metadataNode in metadataList) {
-                        switch (metadataNode.GetAttributeValue("name", String.Empty).ToLowerInvariant()) {
-                            case "layout": {
-                                    layoutRecordName = metadataNode.GetAttributeValue("content", String.Empty);
-                                    break;
-                                }
-                            case "template": {
-                                    templateRecordName = metadataNode.GetAttributeValue("content", String.Empty);
-                                    break;
-                                }
-                            default: {
-                                    break;
-                                }
+                    if (bodyNode == null) {
+                        //
+                        // -- no body found, use entire document
+                        returnStatusMessage += cp.Html.p("The content does not include a <body> tag so the entire document will be imported.");
+                    } else {
+                        //
+                        // -- use body
+                        string body = bodyNode.InnerText;
+                        if (string.IsNullOrWhiteSpace(body)) {
+                            //
+                            // -- body tag not found, import the whole document
+                            returnStatusMessage += cp.Html.p("The content includes a <body> tag, but the body tag is empty.");
+                            return false;
                         }
+                        //
+                        // -- body found, set the htmlDoc to the body
+                        htmlDoc.LoadHtml(body);
                     }
-                    if (string.IsNullOrWhiteSpace(layoutRecordName) && string.IsNullOrWhiteSpace(templateRecordName)) {
-                        returnErrorMessage = "No template or layout name could be determined. If not selected, a target must be included as a meta tag in the html document (<meta name=\"layout\" content=\"LayoutName\"> or <meta name=\"template\" content=\"TemplateName\">)";
-                        return false;
+                    LayoutModel layout = null;
+                    if ((layout == null) && !layoutId.Equals(0)) {
+                        layout = DbBaseModel.create<LayoutModel>(cp, layoutId);
+                        if (layout == null) {
+                            returnStatusMessage += cp.Html.p("The layout selected could not be found.");
+                            return false;
+                        }
+                        layout.layout.content = htmlDoc.DocumentNode.OuterHtml;
+                        layout.save(cp);
                     }
-                    if (!string.IsNullOrWhiteSpace(layoutRecordName)) {
-                        LayoutModel layout = DbBaseModel.createByUniqueName<LayoutModel>(cp, layoutRecordName);
+                    if ((layout == null) && !string.IsNullOrWhiteSpace(layoutRecordName)) {
+                        layout = DbBaseModel.createByUniqueName<LayoutModel>(cp, layoutRecordName);
                         if (layout == null) {
                             layout = DbBaseModel.addDefault<LayoutModel>(cp);
                             layout.name = layoutRecordName;
                         }
-                        layout.layout.content = body;
+                        layout.layout.content = htmlDoc.DocumentNode.OuterHtml;
                         layout.save(cp);
                     }
-                    if (!string.IsNullOrWhiteSpace(templateRecordName)) {
-                        PageTemplateModel template = DbBaseModel.createByUniqueName<PageTemplateModel>(cp, templateRecordName);
+
+
+
+                    PageTemplateModel template  = null;
+                    if ((template == null) && !templateId.Equals(0)) {
+                        template = DbBaseModel.create<PageTemplateModel>(cp, templateId);
+                        if (template == null) {
+                            returnStatusMessage += cp.Html.p("The template selected could not be found.");
+                            return false;
+                        }
+                        template.bodyHTML = htmlDoc.DocumentNode.OuterHtml;
+                        layout.save(cp);
+                    }
+                    if ((template == null) && !string.IsNullOrWhiteSpace(templateRecordName)) {
+                        template = DbBaseModel.createByUniqueName<PageTemplateModel>(cp, templateRecordName);
                         if (template == null) {
                             template = DbBaseModel.addDefault<PageTemplateModel>(cp);
                             template.name = templateRecordName;
                         }
-                        template.bodyHTML = body;
+                        template.bodyHTML = htmlDoc.DocumentNode.OuterHtml;
                         template.save(cp);
+                    }
+
+
+
+
+                    if ((layout==null) && (template == null)) {
+                        returnStatusMessage += cp.Html.p("No template or layout name could be determined. If not selected, a target must be included as a meta tag in the html document (&lt;meta name=\"layout\" content=\"LayoutName\"&gt; or &lt;meta name=\"template\" content=\"TemplateName\"&gt;)");
+                        return false;
                     }
                     //
                     return true;
--- source/aoHtmlImport/Views/HtmlImportClass.cs
+++ source/aoHtmlImport/Views/HtmlImportClass.cs
@@ -22,7 +22,7 @@ namespace Contensive.Addons.HtmlImport {
                         if (!cp.TempFiles.SaveUpload(uploadFormInputName, uploadFolder, ref uploadFile)) {
                             statusMessage = "Upload failed";
                         } else {
-                            ImportController.importHtmlFile(cp, cp.TempFiles.PhysicalFilePath + uploadFolder + uploadFile, cp.Doc.GetText("layoutName"), cp.Doc.GetText("templateName"), ref statusMessage);
+                            ImportController.importHtmlFile(cp, cp.TempFiles.PhysicalFilePath + uploadFolder + uploadFile, cp.Doc.GetInteger("layoutId"), cp.Doc.GetInteger("templateId"), ref statusMessage);
                         }
                     }
                     //
@@ -32,8 +32,8 @@ namespace Contensive.Addons.HtmlImport {
                     form.Warning = (string.IsNullOrWhiteSpace(statusMessage) ? "" : statusMessage);
                     form.Description = cp.Html5.P("This tool uploads and imports html files and converts the html to Mustache-format compatible templates and layouts. See reference at the end of this document.");
                     form.Body += cp.AdminUI.GetEditRow("Html Upload", cp.AdminUI.GetFileEditor(uploadFormInputName, ""), "Select the file you need to import. The file may include directives the determine the save location and Mustache replacements.");
-                    form.Body += cp.AdminUI.GetEditRow("Select Layout", cp.AdminUI.GetLookupContentEditor("layoutName", "Layouts"), "(Optional) Select the Layout you want to populate with this html document. Leave blank if the target is set in a meta tag of the html document.");
-                    form.Body += cp.AdminUI.GetEditRow("Select Template", cp.AdminUI.GetLookupContentEditor("templateName", "Page Templates"), "(Optional) Select the Page Template you want to populate with this html document. Leave blank if the target is set in a meta tag of the html document.","",false,true);
+                    form.Body += cp.AdminUI.GetEditRow("Select Layout", cp.AdminUI.GetLookupContentEditor("layoutId", "Layouts"), "(Optional) Select the Layout you want to populate with this html document. Leave blank if the target is set in a meta tag of the html document.");
+                    form.Body += cp.AdminUI.GetEditRow("Select Template", cp.AdminUI.GetLookupContentEditor("templateId", "Page Templates"), "(Optional) Select the Page Template you want to populate with this html document. Leave blank if the target is set in a meta tag of the html document.","",false,true);
                     form.AddFormButton("Upload");
                     form.AddFormButton("Cancel");
                     return form.GetHtml(cp);
